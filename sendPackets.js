7272064a6ca60f05a8fffbf0b922951e51ee2c70
Monty Dimkpa <cmdimkpa@gmail.com>
2020-06-15 04:30:57 +0100

    Update sendPackets.js

--- sendPackets.js
+++ sendPackets.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3200
 
 let burstSize = 10;
-let burstInterval = 10000;
+let burstInterval = 2500;
 let events = 0;
 
 const sendPackets = async () => {
